6510214c6b373df8eb28ea35b86778cb7076a461
Reed Sweeney <reed.sweeney@gmail.com>
2014-02-19 13:23:26 -0800

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Reed Sweeney on 2/19/14.
+//  Copyright (c) 2014 Reed Sweeney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
